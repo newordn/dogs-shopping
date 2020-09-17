b7edbeb5d90f2e4931e234746709306c46b12720
Neword <newordn@gmail.com>
2020-09-17 20:14:53 +0200

    added Our services section (page 2)

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,35 +1,85 @@
-import React from 'react';
-import './Home.css';
-import {Carousel} from "react-bootstrap";
+import React from "react";
+import "./Home.css";
+import { Carousel, Button, Container, Row, Col, Card } from "react-bootstrap";
 
 export function Home() {
-  return (<div>
-     <Carousel>
-     <Carousel.Item>
-    <img
-      className="d-block w-100 img-fluid img-size"
-      src="/statics/imgs/slide1.jpg"
-      alt="Get your best friend"
-    />
+  return (
+    <div>
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100 img-fluid img-size"
+            src="/statics/imgs/slide1.jpg"
+            alt="Get your best friend"
+          />
 
-    <Carousel.Caption>
-      <h1>Get your best friend</h1>
-      <p>With more than 1000 of pets in your shops</p>
-    </Carousel.Caption>
-  </Carousel.Item>
+          <Carousel.Caption>
+            <h1 className="mb-5">Get your best friend</h1>
+            <p className="mb-3">With more than 1000 of pets in your shops</p>
+            <p>
+              <Button className="bg-red border-none" size="lg">
+                Order Now
+              </Button>
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
 
-  <Carousel.Item>
-    <img
-      className="d-block w-100 img-size"
-      src="/statics/imgs/slide2.jpg"
-      alt="service"
-    />
-    <Carousel.Caption>
-      <h1>Best service for your Pet</h1>
-      <p>We take care of your pet, like never before, he will thank you.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-</Carousel>
-  </div>
+        <Carousel.Item>
+          <img
+            className="d-block w-100 img-size"
+            src="/statics/imgs/slide2.jpg"
+            alt="service"
+          />
+          <Carousel.Caption>
+            <h1 className="mb-5">Best service for your Pet</h1>
+            <p className="mb-3">
+              We take care of your pet, like never before, he will thank you.
+            </p>
+            <p>
+              <Button className="bg-red border-none" size="lg">
+                Order Now
+              </Button>
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+      <div class="services">
+        <Container>
+          <h1 className="text-center bg-red text-white mb-4">Our services</h1>
+          <Row>
+            <Col md={6} className="mb-1">
+              <Card>
+                <Card.Img variant="top" src="/statics/imgs/article1.jpg" />
+                <Card.Body>
+                  <Card.Title>1000 pets available</Card.Title>
+                  <Card.Text>
+                    We have got a collection of more than 1000 pets available
+                    just for you.
+                  </Card.Text>
+                  <Button className="bg-red border-none" size="lg">
+                    Shop Now
+                  </Button>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col md={6} className="mb-1">
+              <Card>
+                <Card.Img variant="top" src="/statics/imgs/article2.jpg" />
+                <Card.Body>
+                  <Card.Title>Washing, Healing, Dental Care</Card.Title>
+                  <Card.Text>
+                    From its quenottes to the ends of its paws, the dog needs
+                    some regular care, We are taking care of it for you.
+                  </Card.Text>
+                  <Button className="bg-red border-none" size="lg">
+                    Shop Now
+                  </Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </div>
   );
 }
