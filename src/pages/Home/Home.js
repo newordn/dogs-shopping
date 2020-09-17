e7d75afe68945fc1498d7107d368aa57b047b785
Neword <newordn@gmail.com>
2020-09-17 17:51:54 +0200

    Added carousel on home page(page 1)

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,35 @@
 import React from 'react';
 import './Home.css';
+import {Carousel} from "react-bootstrap";
 
 export function Home() {
   return (<div>
-      Home
+     <Carousel>
+     <Carousel.Item>
+    <img
+      className="d-block w-100 img-fluid img-size"
+      src="/statics/imgs/slide1.jpg"
+      alt="Get your best friend"
+    />
+
+    <Carousel.Caption>
+      <h1>Get your best friend</h1>
+      <p>With more than 1000 of pets in your shops</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+
+  <Carousel.Item>
+    <img
+      className="d-block w-100 img-size"
+      src="/statics/imgs/slide2.jpg"
+      alt="service"
+    />
+    <Carousel.Caption>
+      <h1>Best service for your Pet</h1>
+      <p>We take care of your pet, like never before, he will thank you.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
   </div>
   );
 }
